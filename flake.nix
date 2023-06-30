96dd79b5f3d723dac0b171d485c56ca1ec90c6b3
Johannes Schickling <schickling.j@gmail.com>
2023-06-30 12:54:22 +0200

    init

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/master";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+
+        # devShell = with pkgs; pkgs.mkShell {
+        #   buildInputs = [
+        #     nodejs_20
+        #     (yarn.override { nodejs = nodejs_20; })
+        #   ];
+        # };
+
+        devShells = {
+          default = with pkgs; pkgs.mkShell {
+            buildInputs = [
+              nodejs_20
+              (yarn.override { nodejs = nodejs_20; })
+            ];
+          };
+        };
+
+      });
+}
